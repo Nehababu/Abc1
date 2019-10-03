4ac7e93249c2f4d61f86f3584a9b7f4c712e9bc3
ankita <ankitakumari650@gmail.com>
2019-10-03 21:23:50 +0530

    removed

--- WebApplication1/WebApplication1/Model/Student.cs
+++ WebApplication1/WebApplication1/Model/Student.cs
@@ -10,9 +10,6 @@ namespace WebApplication1.Model
         public string Name { get; set; }
         public string City { get; set; }
 
-       public string Address { get; set; }
-        public string Mobile { get; set; }
-        public string Branch { get; set; }
-        public string mobile { get; set; }
+      
     }
 }
